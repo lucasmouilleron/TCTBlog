a703c947660ca92e2b0412f0e137db957ff4ac91
lucas mouilleron <lucas.mouilleron@me.com>
2013-06-03 19:50:07 +0200

    jekyll post

--- about.md
+++ about.md
@@ -7,10 +7,11 @@ group : navigation
 {% include JB/setup %}
 
 ##The Creative Technology ?
-TODO
+Ce blog est le fruit de nos recherches digitales, au sein de l'agence [Diplomatic Cover](http://diplomatic-cover.com).
 
-##Me contacter
-- {{site.author.name}}
-- {{site.author.address}}
+Nos champs d'investigations sont l'étude des technologies émergentes à forte valeur ajoutée pour la communication et le marketing digital et l'écoute et l'analyse des tendances technologiques digitales en général.
+
+##{{site.author.name}}
+- Je suis Creative Technologist chez [Diplomatic Cover](http://diplomatic-cover.com) (groupe Publicis)
 - [{{site.author.email}}](mailto:{{site.author.email}})
 - [Profil linkedin](http://lnkd.in/6fN3Ks)
--- assets/themes/ct/js/main.js
+++ assets/themes/ct/js/main.js
@@ -2,6 +2,19 @@
 // MAIN
 //------------------------------------------------------------------------------
 $(function() {
+    setupMenu();
+});
+
+//------------------------------------------------------------------------------
+function setupMenu() {
+    $("header nav").hover(function() {
+        $("header nav.sticky").stop().animate({opacity: 1});
+        $("header nav.sticky *").stop().animate({opacity: 1});
+    }, function() {
+        $("header nav.sticky").stop().animate({opacity: 0.7});
+        $("header nav.sticky *").stop().animate({opacity: 0.7});
+
+    });
     var navOuterHeight = $("header nav").outerHeight();
     var navHeight = $("header nav").height();
     $(".navWrapper").height(navOuterHeight);
@@ -9,16 +22,20 @@ $(function() {
         handler: function(direction) {
             if (direction === "down") {
                 $("header nav").addClass("sticky");
-                $("header nav").css({top: -navHeight, opacity: 0});
-                $("header nav").animate({top: 0, opacity: 1}, 200);
+                $("header nav.sticky").css({opacity: 0.7});
+                $("header nav.sticky *").css({opacity: 0.7});
+                $("header nav").css({top: -navHeight});
+                $("header nav").stop().animate({top: 0}, 200);
 
             } else {
-                $("header nav").fadeOut(300, function() {
+                $("header nav").stop().fadeOut(300, function() {
                     $("header nav").removeClass("sticky");
                     $("header nav").show();
+                    $("header nav *").css({opacity: 1});
+                    $("header nav").css({opacity: 1});
                 });
             }
         },
         offset: navOuterHeight + 20
     });
-});
+}
