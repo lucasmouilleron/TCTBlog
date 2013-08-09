75d2939a53ae9318a4e8eb50cb208d8a13ac34cc
Lucas Mouilleron <lucas.mouilleron@me.com>
2013-08-09 15:37:44 +0200

    font pictos

---
+++ _posts/2013-08-09-font-icons.md
@@ -0,0 +1,31 @@
+---
+layout: post
+title: Polices de pictogrammes
+tags: [responsive, fonts]
+picture: font-icons.jpg
+---
+{% include JB/setup %}
+
+**Si la gestion des pictogrammes d'un site web était déjà une problématique** (transparence sur vieux navigateurs, optimisation du nombre de requêtes via sprite sheets css, plusieurs versions par taille, etc.), les écrans à haute densité à la Rétina d'Apple confirment **la nécessité d'une solution cross-browser, cross-scale, et optimisée pour le réseau.**
+
+##Polices de pictogrammes
+L'idée est de générer une police de pictogrammes (a = mail, b = ampoule, c = lion, etc.) pour bénéficier des avantges d'une police : 
+
+- Insensible à la taille
+- Insensible aux densités
+- Transparence et ombre cross-browser
+- Un seul fichier pour tous les charactères
+- Manipulation CSS : opacité, rotation, etc.
+
+##Implémentation
+- HTML : `<h3><span aria-hidden="true" data-icon="&#x21dd;"></span> Stats</h3>`
+- CSS : `[data-icon]:before {  font-family: icons;  content: attr(data-icon);  speak: none;}`
+- Génération de la police : à partir d'un logiciel de typographie, ou depuis des services en lignes gratuits ou payants
+
+##Limites
+- Encore quelques hacks à mettre en place pour l'accessiblité et la lecture à voix haute
+- Nécessite un workflow fluide et industrielle entre la création et la production
+
+##Ressources :
+- [Article CSS Tricks](http://css-tricks.com/examples/IconFont)
+- [Fontello.com, génération de pictogrammes](http://fontello.com)
--- assets/themes/ct/js/main.js
+++ assets/themes/ct/js/main.js
@@ -7,14 +7,13 @@ $(function() {
 
 //------------------------------------------------------------------------------
 function setupMenu() {
-    $("header nav").hover(function() {
+    /*$("header nav").hover(function() {
         $("header nav.sticky").animate({opacity: 1});
         $("header nav.sticky *").animate({opacity: 1});
     }, function() {
-        $("header nav.sticky").animate({opacity: 0.9});
-        $("header nav.sticky *").animate({opacity: 0.9});
-
-    });
+        $("header nav.sticky").animate({opacity: 1});
+        $("header nav.sticky *").animate({opacity: 1});
+    });*/
     var navOuterHeight = $("header nav").outerHeight();
     var navHeight = $("header nav").height();
     $(".navWrapper").height(navOuterHeight);
@@ -22,8 +21,8 @@ function setupMenu() {
         handler: function(direction) {
             if (direction === "down") {
                 $("header nav").addClass("sticky");
-                $("header nav.sticky").css({opacity: 0.9});
-                $("header nav.sticky *").css({opacity: 0.9});
+                //$("header nav.sticky").css({opacity: 0.9});
+                //$("header nav.sticky *").css({opacity: 0.9});
                 $("header nav").css({top: -navHeight});
                 $("header nav").animate({top: 0}, 200, function() {
                     $("header nav").css({top: 0});
@@ -33,8 +32,8 @@ function setupMenu() {
                 $("header nav").fadeOut(200, function() {
                     $("header nav").removeClass("sticky");
                     $("header nav").show();
-                    $("header nav *").css({opacity: 1});
-                    $("header nav").css({opacity: 1});
+                    //$("header nav *").css({opacity: 1});
+                    //$("header nav").css({opacity: 1});
                 });
             }
         },
